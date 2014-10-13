edbb59c80ae373ca3e1748281174bf438dfcbed4
Michael Yong <wrong92@gmail.com>
2014-10-13 15:46:56 +0800

    Fix laplace smoothing.

--- src/LaplaceModel.java
+++ src/LaplaceModel.java
@@ -1,19 +1,19 @@
 public class LaplaceModel extends BasicModel {
 	Long allPossibleWordTagTypes;
 	Long allPossibleTagTagTypes;
-	public static final double DISCOUNT = 0.5;
+	public static final double DISCOUNT = 0.1;
 
 	public LaplaceModel(Language language) {
 		super(language);
 	}
-	
+
 	private Long getAllPossibleTagTagTypes() {
 		if (allPossibleTagTagTypes == null) {
 			allPossibleTagTagTypes = (long) Math.pow(this.language.getTagTypeCount(), 2);
 		}
 		return allPossibleTagTagTypes;
 	}
-	
+
 	private Long getAllPossibleWordTagTypes() {
 		if (allPossibleWordTagTypes == null) {
 			allPossibleWordTagTypes = (long) this.language.getTagTypeCount() * this.language.getWordTypeCount();
@@ -24,12 +24,12 @@ public class LaplaceModel extends BasicModel {
 	@Override
 	public double getProbabilityOfWordGivenTag(String w, String t) {
 		return (double) this.language.getWordTagCount(w, t) + DISCOUNT /
-			(this.language.getTagCount(t) + getAllPossibleWordTagTypes());
+			(this.language.getTagCount(t) + (DISCOUNT * getAllPossibleWordTagTypes()));
 	}
 
 	@Override
 	public double getProbabilityOfNextTagGivenTag(String t2, String t1) {
 		return (double) this.language.getTagTagCount(t1, t2) + DISCOUNT /
-			(this.language.getTagCount(t1) + getAllPossibleTagTagTypes());
+			(this.language.getTagCount(t1) + (DISCOUNT * getAllPossibleTagTagTypes()));
 	}
 }
