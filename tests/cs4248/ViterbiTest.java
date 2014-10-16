d92db27bb7bba56db68866516ce7bf1d836ec7e1
Michael Yong <wrong92@gmail.com>
2014-10-16 19:26:34 +0800

    Add another test for viterbi algorithm.

--- tests/cs4248/ViterbiTest.java
+++ tests/cs4248/ViterbiTest.java
@@ -16,11 +16,11 @@ public class ViterbiTest {
 	@Before
 	public void setUp() {
 		model = mock(IModel.class);
-		viterbi = new Viterbi(model, new String[]{"N", "V"}, "water plants");
 	}
 
 	@Test
-	public void testViterbiAlgorithm() {
+	public void testViterbiAlgorithm1() {
+		viterbi = new Viterbi(model, new String[]{"N", "V"}, "water plants");
 		stubTagTag("N", "N", (double) 1/5);
 		stubTagTag("N", "V", (double) 1/2);
 		stubTagTag("V", "N", (double) 3/5);
@@ -38,6 +38,64 @@ public class ViterbiTest {
 		stubWordTag("plants", "V", (double) 1/20);
 
 		assertEquals(Math.log((double) 3/5000), viterbi.getMaxProbability(), 0.000001 /* delta */);
+		assertEquals("water/V plants/N", viterbi.getTaggedSentence());
+	}
+	
+	@Test
+	public void testViterbiAlgorithm2() {
+		viterbi = new Viterbi(model, new String[]{"A", "B", "C", "D"}, "he is watering plants");
+		stubTagTag("A", "A", (double) 1/5);
+		stubTagTag("A", "B", (double) 2/5);
+		stubTagTag("A", "C", (double) 4/5);
+		stubTagTag("A", "D", (double) 2/5);
+
+		stubTagTag("B", "A", (double) 1/5);
+		stubTagTag("B", "B", (double) 2/5);
+		stubTagTag("B", "C", (double) 4/5);
+		stubTagTag("B", "D", (double) 2/5);
+
+		stubTagTag("C", "A", (double) 1/5);
+		stubTagTag("C", "B", (double) 1/5);
+		stubTagTag("C", "C", (double) 1/5);
+		stubTagTag("C", "D", (double) 1/5);
+
+		stubTagTag("D", "A", (double) 1/5);
+		stubTagTag("D", "B", (double) 1/5);
+		stubTagTag("D", "C", (double) 1/5);
+		stubTagTag("D", "D", (double) 1/5);
+
+		stubStartTag("A", (double) 2/5);
+		stubStartTag("B", (double) 3/5);
+		stubStartTag("C", (double) 3/5);
+		stubStartTag("D", (double) 3/5);
+
+		stubEndTag("A", (double) 1/10);
+		stubEndTag("B", (double) 2/10);
+		stubEndTag("C", (double) 2/10);
+		stubEndTag("D", (double) 1/10);
+
+		stubWordTag("he", "A", (double) 1/20);
+		stubWordTag("he", "B", (double) 1/20);
+		stubWordTag("he", "C", (double) 1/20);
+		stubWordTag("he", "D", (double) 1/20);
+
+		stubWordTag("is", "A", (double) 1/20);
+		stubWordTag("is", "B", (double) 1/20);
+		stubWordTag("is", "C", (double) 1/20);
+		stubWordTag("is", "D", (double) 1/20);
+
+		stubWordTag("watering", "A", (double) 1/20);
+		stubWordTag("watering", "B", (double) 1/20);
+		stubWordTag("watering", "C", (double) 1/20);
+		stubWordTag("watering", "D", (double) 1/20);
+
+		stubWordTag("plants", "A", (double) 1/20);
+		stubWordTag("plants", "B", (double) 1/20);
+		stubWordTag("plants", "C", (double) 1/20);
+		stubWordTag("plants", "D", (double) 1/20);
+
+
+//		assertEquals(Math.log((double) 3/5000), viterbi.getMaxProbability(), 0.000001 /* delta */);
 		assertEquals("water/V plants/N", viterbi.getTaggedSentence());
 	}
 
